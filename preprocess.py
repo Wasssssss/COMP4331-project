875d7c9f30e16eeacc81113e37e9c6c9bfe64b46
Brown <31978604+gilliankwk@users.noreply.github.com>
2018-11-26 11:50:47 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -56,3 +56,61 @@ if __name__ == '__main__':
 #                    if enrollment.enrollment_info(k)[2] == objects.object_info.get(j)[0]:
                 print(objects.object_info.get(i),date.course_info.get(i)[2])
 
+    # for k in log.enrollment_ids:
+        # print(k)
+        # print(log.enrollment_info.get(k)[1])
+
+    print(log.enrollment_info.get("4")[0]) # 4
+    print(log.enrollment_info.get("4")[1][1][:10]) # 2014-06-15
+
+
+# ------------- meaningful data, grouped by each user ------------- #
+
+    # count no. of different events by each user
+    events = {}
+    for key in log.enrollment_ids: # Log_Data.events.keys():
+        countproblem = 0
+        countvideo = 0 
+        countaccess = 0
+        countwiki = 0 
+        countdiscussion = 0
+        countnavigate = 0
+        countpage_close = 0
+        for event in log.events.get(key): # Log_Data.events.get(key):
+            if event == "problem":
+                countproblem += 1
+            elif event == "video":
+                countvideo += 1
+            elif event == "access":
+                countaccess += 1
+            elif event == "wiki":
+                countwiki += 1
+            elif event == "discussion":
+                countdiscussion += 1
+            elif event == "navigate":
+                countnavigate += 1
+            elif event == "page_close":
+                countpage_close += 1
+            else:
+                print("Error")
+        events[key]= {countproblem, countvideo, countaccess, countwiki, countdiscussion, countnavigate, countpage_close}
+    print(events)
+
+    # count no. of access per day by each user
+    period = {}
+    for key in log.enrollment_ids:
+        period[key] =len(log.dates.get(key))
+    print(period)
+
+    # find the latest access by each user
+    latest_access = {}
+    for key in log.enrollment_ids:
+        latest_access[key] = max(log.dates.get(key))
+    print(latest_access)
+
+# ------------- www ------------- #
+# >>> a = np.array([[1, 2], [3, 4]])
+# >>> b = np.array([[5, 6]])
+# >>> np.concatenate((a, b), axis=0)
+
+# arr = np.array([])
